17407bb12a3f46feed82fe6290cc017eec5cc84e
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-10 05:05:31 +0300

    Create Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Main from "./components/Main"
+import Projects from "./components/Projects"
 import Sidenav from "./components/Sidenav"
 import Work from "./components/Work"
 
@@ -10,6 +11,7 @@ function App() {
         <Sidenav />
         <Main />
         <Work />
+        <Projects />
     </>
   )
 }
